5152bd0954cc0dacb1a0ff1f42a49cf64f7b4bd3
Boris <fjmiranda009@gmail.com>
2020-04-07 15:45:37 -0600

    121 conectando con api

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/src/models/pelicula_models.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
 
 class DataSearch extends SearchDelegate {
   String seleccion = '';
@@ -47,26 +49,71 @@ class DataSearch extends SearchDelegate {
   @override
   Widget buildSuggestions(BuildContext context) {
     // TODO: son las sugerencias que aparecen cuando las personas escriben
+    if(query.isEmpty){
+      return Container();
+    }
+    PeliculaProvider peli = new PeliculaProvider();
+    return FutureBuilder(
+        future: peli.buscarPelicula(query),
 
+        builder: (BuildContext context, AsyncSnapshot<List<Pelicula>> snapshot) {
+          if (snapshot.hasData) {
+            if (snapshot.data != null) {
+              final peliculaSeleccionada = snapshot.data;
+              return new ListView(
+                children:
+                  peliculaSeleccionada.map((mypeli){
+                    return new ListTile(
+                      leading: FadeInImage(
+                        image: NetworkImage(mypeli.getPosterImg()),
+                        placeholder: AssetImage('assets/img/no-image.jpg'),
+                        width: 50.0,
+                        fit: BoxFit.contain,
+                      ),
+                      title: new Text(mypeli.title),
+                      subtitle: new Text(mypeli.originalTitle),
+                      onTap: (){
+                        close(context, null);
+                        mypeli.uniqueId ='';
+                        Navigator.pushNamed(context, 'detalle', arguments: mypeli);
+                      },
+                    );
+                  }).toList()
+                ,
+              );
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          }else{
+            return Center(child: CircularProgressIndicator());
+          }
+          }
+          );
+
+
+  }
+
+  respaldoCortado(){
     final listaSugerida = (query.isEmpty)?
     peliculasRecientes : peliculas.where((p){
       return p.toLowerCase().startsWith(query.toLowerCase());
     }).toList();
 
     return new ListView.builder(
-        itemBuilder: (context, index){
-          return ListTile(
-            leading: Icon(Icons.movie),
-            title: new Text(listaSugerida[index]),
-            onTap: (){
-              seleccion =listaSugerida[index];
-              showResults(context);
-            },
-          );
-        },
-        itemCount: listaSugerida.length,
+      itemBuilder: (context, index){
+        return ListTile(
+          leading: Icon(Icons.movie),
+          title: new Text(listaSugerida[index]),
+          onTap: (){
+            seleccion =listaSugerida[index];
+            showResults(context);
+          },
+        );
+      },
+      itemCount: listaSugerida.length,
 
 
     );
   }
+
 }
